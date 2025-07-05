4b65d9bc3da67988db3015b60f6f04e3ff13b548
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-05 14:58:30 +0100

    added value for db table and key

--- backend.tf
+++ backend.tf
@@ -1,9 +1,10 @@
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = ""
-    key     = ""
+    bucket  = "kiddo-terraform-remote-state-files"
+    key     = "dynamic-webapp.tfstate"
     region  = "us-east-1"
+    dynamodb_table = "terraform-state-lock"
     profile = "terraform-user"
   }
 }
