8e0abc2fbe4ee7dc26501e0b29fe1afae176d741
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-05 14:36:04 +0100

    updated variable.tf file

--- variables.tf
+++ variables.tf
@@ -1,5 +1,6 @@
 variable"region" {}
 variable "project_name"{}
+variable "domain_name" {}
 variable "vpc_cidr" {}
 variable "public_subnet_az1_cidr" {}
 variable "public_subnet_az2_cidr" {}
@@ -7,3 +8,12 @@ variable "private_app_subnet_az1_cidr" {}
 variable "private_app_subnet_az2_cidr" {}
 variable "private_data_subnet_az1_cidr" {}
 variable "private_data_subnet_az2_cidr" {}
+variable "internet_gateway" {}
+variable "vpc_id" {}
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "alternative_name" {}
