51a73e2da9600c4a80feb3cd71b36a9a6ad5bb2f
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-12 15:00:15 +0100

    update

--- ecs.tf
+++ ecs.tf
@@ -1,8 +1,19 @@
+# create ecs cluster
 resource "aws_ecs_cluster" "ecs_cluster" {
   name = "${jupiter}-cluster"
 
   setting {
     name  = "containerInsights"
     value = "disabled"
+  }
+}
+
+# create cloudwatch log group
+resource "aws_cloudwatch_log_group" "log_group" {
+  name = "${jupiter}-task-definition"
+  retention_in_days = 7
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
