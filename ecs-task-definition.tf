59250a7488255e640f863e82e37550d3cfbb5ef6
Oluwasegun Adepetu <Oluwasegunadepetu@gmail.com>
2025-07-12 15:00:33 +0100

    update

--- ecs-task-definition.tf
+++ ecs-task-definition.tf
@@ -1,10 +1,10 @@
 resource "aws_ecs_task_definition" "ecs_task_definition" {
-  family                    = "${}-task-definition"
-  execution_role_arn        = 
-  network_mode              = 
-  requires_compatibilities  = []
-  cpu                       = 
-  memory                    = 
+  family                    = "${var.project_name}-task-definition"
+  execution_role_arn        = aws_iam_role.ecs_tasks_execution_role.arn
+  network_mode              = "awsvpc"
+  requires_compatibilities  = ["FARGATE"]
+  cpu                       = "256"
+  memory                    = "512"
 
   runtime_platform {
     operating_system_family = "LINUX"
@@ -13,14 +13,14 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
 
   container_definitions     = jsonencode([
     {
-      name                  = "${}-container"
-      image                 = 
+      name                  = "${var.project_name}-container"
+      image                 = var.container_image
       essential             = true
 
       portMappings          = [
         {
-          containerPort     = 
-          hostPort          = 
+          containerPort     = 80
+          hostPort          = "tcp"
         }
       ]
       
@@ -35,9 +35,9 @@ resource "aws_ecs_task_definition" "ecs_task_definition" {
       logConfiguration = {
         logDriver   = "awslogs",
         options     = {
-          "awslogs-group"          = 
-           "awslogs-region"        = 
-          "awslogs-stream-prefix"  = 
+          "awslogs-group"          = aws_cloudwatch_log_group.log_group.name,
+           "awslogs-region"        = "us-east-1",
+          "awslogs-stream-prefix"  = "ecs"
         }
       }
     }
